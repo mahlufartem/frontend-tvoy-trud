9481cfc3a77b5c05cb4dba1133e63827603bd38c
artem.m <artem.m@profilancegroup.com>
2026-01-10 18:49:53 +0300

    Банер секции каталога вакансий

--- src/pages/MainPage/sections/Section_4/Section_4.jsx
+++ src/pages/MainPage/sections/Section_4/Section_4.jsx
@@ -1,4 +1,7 @@
+import Image from 'next/image'
 import React from 'react'
+
+import { mainBanner2 } from '@/assets/images'
 
 import VacanciesCarouselMin from '@/components/VacanciesCarouselMin/VacanciesCarouselMin'
 
@@ -8,9 +11,16 @@ const Section_4 = () => {
 	return (
 		<div className={styles.root}>
 			<VacanciesCarouselMin />
-			<div className={styles.baner}>
-				<div className={styles.content}></div>
-				<div className={styles.image}></div>
+			<div className={styles.bannerWrapper}>
+				<div className={styles.banner}>
+					<div className={styles.content}></div>
+					<div className={styles.image}>
+						<Image
+							src={mainBanner2}
+							alt=''
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
