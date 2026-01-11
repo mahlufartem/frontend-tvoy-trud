6f1287e65f9289720eaf06a4eb5e0a07abcce5e5
artem.m <artem.m@profilancegroup.com>
2026-01-11 16:53:40 +0300

    Банер секции каталога вакансий

--- src/pages/MainPage/sections/Section_4/Section_4.jsx
+++ src/pages/MainPage/sections/Section_4/Section_4.jsx
@@ -1,22 +1,38 @@
+'use client'
+
 import Image from 'next/image'
 import React from 'react'
 
-import { mainBanner2 } from '@/assets/images'
+import { useWindowWidth } from '@/hooks/useWindowWidth'
+
+import { mainBanner2, mainBanner2_mini } from '@/assets/images'
 
 import VacanciesCarouselMin from '@/components/VacanciesCarouselMin/VacanciesCarouselMin'
 
 import styles from './Section_4.module.scss'
 
 const Section_4 = () => {
+	const width = useWindowWidth()
+
 	return (
 		<div className={styles.root}>
 			<VacanciesCarouselMin />
 			<div className={styles.bannerWrapper}>
 				<div className={styles.banner}>
-					<div className={styles.content}></div>
+					<div className={styles.content}>
+						<div className={styles.text}>
+							<p className={styles.title}>
+								Не знаете, какая вакансия подойдет вам больше?
+							</p>
+							<p className={styles.subtitle}>
+								Заполните анкету, мы позвоним вам и поможем с выбором
+							</p>
+						</div>
+						<button className={styles.btn}> Зыполните анкету</button>
+					</div>
 					<div className={styles.image}>
 						<Image
-							src={mainBanner2}
+							src={width <= 1023 ? mainBanner2_mini : mainBanner2}
 							alt=''
 						/>
 					</div>
