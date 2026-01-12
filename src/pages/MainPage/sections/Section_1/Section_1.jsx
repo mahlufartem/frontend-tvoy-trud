47b260d03f7cf594fcd5f4b70f1d214dd4257e26
artem.m <artem.m@profilancegroup.com>
2026-01-12 18:41:57 +0300

    Превый баннер для главной страницы

--- src/pages/MainPage/sections/Section_1/Section_1.jsx
+++ src/pages/MainPage/sections/Section_1/Section_1.jsx
@@ -1,11 +1,18 @@
 'use client'
 
+import Image from 'next/image'
 import React from 'react'
 
 import 'swiper/css'
 import 'swiper/css/pagination'
 import { Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
+
+import {
+	mainBannerFormsL,
+	mainBannerFormsXL,
+	mainBannerPerson
+} from '@/assets/images'
 
 import styles from './Section_1.module.scss'
 
@@ -20,7 +27,31 @@ const Section_1 = () => {
 				navigation
 				pagination={{ clickable: true }}
 			>
-				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>
+					<div className={styles.banner}>
+						<div className={styles.left}></div>
+						<div className={styles.right}>
+							<div className={styles.formImage}>
+								<Image
+									src={mainBannerFormsL}
+									alt=''
+								/>
+							</div>
+							<div className={styles.personImage}>
+								<Image
+									src={mainBannerPerson}
+									alt=''
+								/>
+							</div>
+							<div className={styles.personInfo}>
+								<span className={styles.personInfo__title}>Грузчик</span>
+								<span className={styles.personInfo__price}>
+									до 2 800 ₽ за смену
+								</span>
+							</div>
+						</div>
+					</div>
+				</SwiperSlide>
 				<SwiperSlide>Slide 2</SwiperSlide>
 				<SwiperSlide>Slide 3</SwiperSlide>
 				<SwiperSlide>Slide 4</SwiperSlide>
