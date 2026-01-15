ced15d64e0c0bcdbf1ea8af8d2429167569770eb
artem.m <artem.m@profilancegroup.com>
2026-01-15 19:10:26 +0300

    фикс билда

--- src/app/vacancy/[id]/page.jsx
+++ src/app/vacancy/[id]/page.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { vacancies } from '@/components/VacanciesCarousel/data'
 
 const Page = () => {
+	export async function generateStaticParams() {
+		
+		// В дальнейшем заменить на реальны запрос списка вакансий
+		return vacancies.map((vac) => ({
+			id: vac.id
+		}));
+	}
 	return <div></div>
 }
 
