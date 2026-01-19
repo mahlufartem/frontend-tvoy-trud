d5aa458a8fe8a84f4730ef6c4e6e66861c5c0f4d
artem.m <artem.m@profilancegroup.com>
2026-01-19 19:20:10 +0300

    Страница акции

--- src/components/EventsCarousel/EventsCarousel.jsx
+++ src/components/EventsCarousel/EventsCarousel.jsx
@@ -54,7 +54,7 @@ const EventsCarousel = ({ events }) => {
 							>
 								<div className={styles.image}></div>
 								<div className={styles.info}>
-									<div className={styles}>
+									<div className={styles.date}>
 										{date} <ArrowRightTopIcon />
 									</div>
 									<span>{title}</span>
