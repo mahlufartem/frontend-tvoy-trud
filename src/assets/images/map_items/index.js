5d55a38240d6dae23531bdebf105d2636a0181f0
artem.m <artem.m@profilancegroup.com>
2026-01-26 16:48:24 +0300

    анимация для карты

---
+++ src/assets/images/map_items/index.js
@@ -0,0 +1,3 @@
+export { default as mapItem_1 } from './mapItem_1.png'
+export { default as mapItem_2 } from './mapItem_2.png'
+export { default as mapItem_3 } from './mapItem_3.png'
--- src/screens/MainPage/sections/Section_3/Section_3.jsx
+++ src/screens/MainPage/sections/Section_3/Section_3.jsx
@@ -4,11 +4,20 @@ import Image from 'next/image'
 import React from 'react'
 
 import { mapPlag } from '@/assets/images'
+import { mapItem_1, mapItem_2, mapItem_3 } from '@/assets/images/map_items'
 
 import SearchInput from '@/shared/ui/SearchInput/SearchInput'
 
 import styles from './Section_3.module.scss'
 
+const mapItems = [
+	{ id: 1, image: mapItem_1 },
+	{ id: 2, image: mapItem_3 },
+	{ id: 3, image: mapItem_2 },
+	{ id: 4, image: mapItem_1 },
+	{ id: 5, image: mapItem_3 },
+	{ id: 6, image: mapItem_2 }
+]
 const Section_3 = () => {
 	return (
 		<section className={styles.root}>
@@ -35,6 +44,23 @@ const Section_3 = () => {
 						priority
 						style={{ objectFit: 'cover' }}
 					/>
+
+					<div className={styles.mapItems}>
+						<div className={styles.mapItems}>
+							{mapItems.map((item, index) => (
+								<div
+									key={item.id}
+									className={`${styles.mapItem} ${styles[`item_${index + 1}`]}`}
+								>
+									<Image
+										src={item.image}
+										alt=''
+										fill
+									/>
+								</div>
+							))}
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
